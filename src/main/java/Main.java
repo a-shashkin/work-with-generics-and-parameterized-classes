4c711e3b3c4d90b3cfe52192c5576b709059e8ea
a-shashkin <caezar1993@gmail.com>
2023-06-13 23:25:22 +0400

    Added addFruitToBasket method to Main (must find a way to rewrite it to get rid of "Unchecked cast" warning.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,4 +3,8 @@ public class Main {
     public static void main(String[] args) {
 
     }
+
+    public <T extends Fruit> void addFruitToBasket(Fruit fruit, Basket<T> basket) {
+        basket.basketContent.add((T) fruit);
+    }
 }
