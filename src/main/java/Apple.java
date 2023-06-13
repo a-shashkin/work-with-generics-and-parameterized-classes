c5a70179af3ea8e6f2249521b9a05b9c1691f47b
a-shashkin <caezar1993@gmail.com>
2023-06-13 12:44:12 +0400

    Added gitignore, build.gradle, and classes Main, Fruit, Apple, Orange

---
+++ src/main/java/Apple.java
@@ -0,0 +1,8 @@
+public class Apple extends Fruit {
+
+    private Number weight = 1;
+
+    public Number getFruitWeight() {
+        return weight;
+    }
+}
---
+++ src/main/java/Orange.java
@@ -0,0 +1,8 @@
+public class Orange extends Fruit {
+
+    private Number weight = 1.5;
+
+    public Number getFruitWeight() {
+        return weight;
+    }
+}
